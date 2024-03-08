ef478b7e56088a258a327912954c0b8025a90b5c
DimaSurdiy <dsurdiy@ukr.net>
2024-03-08 18:38:46 +0200

    added scroll function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 "use strict"
 
+window.addEventListener("scroll", pageScroll);
 document.addEventListener("click", documentActions);
+
+function pageScroll() {
+   scrollY > 10
+      ? document.querySelector(".header").classList.add("scroll")
+      : document.querySelector(".header").classList.remove("scroll");
+}
 
 function documentActions(e) {
    const targetElement = e.target;
